86f3814807d3e121c96b1330f8803e652cac395c
Gary Norton <gary.norton@odindev.com>
2020-09-29 08:08:57 -0600

    Add base16-shell submodule to repo

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.dotfiles/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
+ln -s "${ZDOTDIR:-$HOME}/.dotfiles/vim" ${ZDOTDIR:-$HOME}/.vim
+
+git submodule update --init --recursive
