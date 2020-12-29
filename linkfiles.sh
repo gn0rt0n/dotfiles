7f383d701466799af503aaeb5bb731fe0ec6c1c7
Gary Norton <gary.norton@odindev.com>
2020-12-29 11:10:27 -0700

    Update Link Files script

--- linkfiles.sh
+++ linkfiles.sh
@@ -5,8 +5,8 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.dotfiles/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-ln -s "${ZDOTDIR:-$HOME}/.dotfiles/vim" ${ZDOTDIR:-$HOME}/.vim
-ln -s "${ZDOTDIR:-$HOME}/.dotfiles/bin" ${ZDOTDIR:-$HOME}/bin
-ln -s "${ZDOTDIR:-$HOME}/.dotfiles/bin" ${ZDOTDIR:-$HOME}/.iterm2
+# ln -s "${ZDOTDIR:-$HOME}/.dotfiles/vim" ${ZDOTDIR:-$HOME}/.vim
+# ln -s "${ZDOTDIR:-$HOME}/.dotfiles/bin" ${ZDOTDIR:-$HOME}/bin
+# ln -s "${ZDOTDIR:-$HOME}/.dotfiles/bin" ${ZDOTDIR:-$HOME}/.iterm2
 
 git submodule update --init --recursive
